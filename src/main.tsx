47fad401fcab42284ffb435d2e1276cb6c536126
Vivek Sawji <168280120+vivekx11@users.noreply.github.com>
2026-04-11 20:48:45 +0530

    Correct typo in main.tsx
    Fix typo in comment for starting the app

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 // main.tsx 
-
+// stsrt app 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
